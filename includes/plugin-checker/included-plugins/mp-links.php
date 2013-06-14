72dd314d01510f4b26f4c81e92c197df9be52b82
Phil Johnston <johnstonphilip@gmail.com>
2013-06-14 14:51:33 -0400

    _admin_menu plugin checker update
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/plugin-checker/included-plugins/mp-links.php
+++ includes/plugin-checker/included-plugins/mp-links.php
@@ -16,5 +16,5 @@
 		$mp_links_plugin_check = new MP_CORE_Plugin_Checker($args);
 	}
  }
-add_action( 'admin_init', 'mp_links_plugin_check' );
+add_action( '_admin_menu', 'mp_links_plugin_check' );
 
