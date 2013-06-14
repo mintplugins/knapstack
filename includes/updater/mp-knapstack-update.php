73de93799d88973b10f12b6590b3395cd740f1cf
Phil Johnston <johnstonphilip@gmail.com>
2013-06-14 02:04:08 -0400

    1.0.0.1
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/updater/mp-knapstack-update.php
+++ includes/updater/mp-knapstack-update.php
@@ -3,16 +3,16 @@
  * Check for updates for this Theme
  *
  */
- if (!function_exists('mt_malachi_software_check')){
-	function mt_malachi_software_check() {
+ if (!function_exists('mp_knapstack_software_check')){
+	function mp_knapstack_software_check() {
 		$args = array(
-			'software_name' => 'Malachi Theme', //<- The name of this theme in the style.css file, mp_repo, and edd. The slug must also match when URL converted (malachi-theme)
+			'software_name' => 'Knapstack Theme', //<- The name of this theme in the style.css file, mp_repo, and edd. The slug must also match when URL converted (malachi-theme)
 			'software_api_url' => 'http://repo.moveplugins.com/',//The URL where EDD and mp_repo are installed and checked
 			'software_licensed' => true, //<-Boolean
 		);
 		
 		//Since this is a theme, call the Theme Updater class
-		$mt_malachi_theme_updater = new MP_CORE_Theme_Updater($args);
+		$mp_knapstack_theme_updater = new MP_CORE_Theme_Updater($args);
 	}
  }
-add_action( 'init', 'mt_malachi_software_check' );
+add_action( 'init', 'mp_knapstack_software_check' );
