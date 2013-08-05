196615b9d98473977c7d0de878506cc408f0ce63
Phil Johnston <johnstonphilip@gmail.com>
2013-08-05 16:11:25 -0400

    1.0.1.0
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="profile" href="//gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
