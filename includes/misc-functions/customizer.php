d0f1794a85bc607269b96110b075291593bda31d
Phil Johnston <johnstonphilip@gmail.com>
2013-08-25 17:57:44 -0400

    1.0.1.6
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/misc-functions/customizer.php
+++ includes/misc-functions/customizer.php
@@ -144,7 +144,7 @@ function mp_knapstack_customizer(){
 					'type'       => 'color',
 					'default'    => '',
 					'priority'   => 1,
-					'element'    => 'input[type=submit], .button, .edd-submit',
+					'element'    => 'input[type=submit], .button, .edd-submit.button',
 					'jquery_function_name' => 'css',
 					'arg' => 'background-color'
 				),
@@ -153,7 +153,7 @@ function mp_knapstack_customizer(){
 					'type'       => 'color',
 					'default'    => '',
 					'priority'   => 1,
-					'element'    => 'input[type=submit]:hover, .button:hover, .edd-submit:hover',
+					'element'    => 'input[type=submit]:hover, .button:hover, .edd-submit.button:hover',
 					'jquery_function_name' => 'css',
 					'arg' => 'background-color'
 				),
