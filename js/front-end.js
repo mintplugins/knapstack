d6029ec67524ee09d77e7114f0c42899fd5417f7
Phil Johnston <johnstonphilip@gmail.com>
2014-01-24 12:38:46 -0500

    1.0.3.7
    Logo Responsive resize
    header responsive resize
    header position for new WP admin bar

--- js/front-end.js
+++ js/front-end.js
@@ -3,10 +3,18 @@
  */
 jQuery(document).ready(function($){
 	
-	//Get height of header - this is dynamic based on the user's logo size
-	var header_height = $('#masthead').height();
+	function mp_knapstack_resize_header(){
+		//Get height of header - this is dynamic based on the user's logo size
+		var header_height = $('#masthead').height();
+		
+		//Set top padding for content based on height of header
+		$('#main-container').css('padding-top', header_height );
+	}
 	
-	//Set top padding for content based on height of header
-	$('#main-container').css('padding-top', header_height );
+	mp_knapstack_resize_header();
+	
+	$(window).on('resize', function(){
+		mp_knapstack_resize_header();
+	});
 	
 });
