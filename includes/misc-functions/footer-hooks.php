4d4461add7b9b4290645225b9594df366814ef31
Phil Johnston <johnstonphilip@gmail.com>
2014-03-08 18:35:48 -0500

    1.0.5.4
    = 1.0.5.4 = March 8, 2014
    * Hide Empty Footer stack if “none” selected
    
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/misc-functions/footer-hooks.php
+++ includes/misc-functions/footer-hooks.php
@@ -7,7 +7,7 @@ function mp_knapstack_footer_stack_callback(){
 	
 	$footer_stack_id = get_theme_mod('mp_knapstack_footer_stack'); 
 	
-	if ( !empty( $footer_stack_id  ) ){
+	if ( !empty( $footer_stack_id  ) && $footer_stack_id != 'none' ){
 		echo mp_stack( get_theme_mod('mp_knapstack_footer_stack') );
 	}
 }
