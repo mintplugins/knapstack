039b72a0f41862e0f27897461b1e1921e3add925
Phil Johnston <support@mintplugins.com>
2015-01-04 21:41:00 -0500

    1.0.7.0 Version Bump
    * Added Sidebar Support
    * Made Knapstack updates free
    * Moved Comments directly below blog content - no longer has its own area.
    * Updated Theme Screenshot
    * Add Tracking upon installation
    * Added Microformats
    * Fixed bug with footer CSS breaking pages in some situations. Reset post data in enqueuescripts
    * Removed “Edit Page” from bottom of Stack Page Template
    * Added Open Graph sharing meta tags
    * Header is absolute by default instead of fixed

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 */
 // Theme version
 if( !defined( 'MP_KNAPSTACK_VERSION' ) )
-	define( 'MP_KNAPSTACK_VERSION', '1.0.6.3' );
+	define( 'MP_KNAPSTACK_VERSION', '1.0.7.0' );
 
 // Theme Folder Path
 if( !defined( 'MP_KNAPSTACK_THEME_DIR' ) )
