8e435cfe1b890bb98904a4d8d268df7a3a4a3f77
Phil Johnston <support@mintplugins.com>
2015-05-13 18:50:50 -0400

    Added post format checking for full width

--- includes/misc-functions/ajax-callbacks.php
+++ includes/misc-functions/ajax-callbacks.php
@@ -2,15 +2,30 @@
 
 //Ajax function which is used to get the list of page templates and show them to use user on the front end
 function mp_knapstack_get_page_templates(){
+	
+	$post_type = $_POST['post_type'];
+	
+	//If this is a page
+	if ( $post_type == 'page' ){
+		echo '<option value="default">' . __( 'MP Stack - 100% Width', 'mp_knapstack' ) . '</option>';	
+		$templates = get_page_templates();
 		
-	$templates = get_page_templates();
+		foreach ( $templates as $template_name => $template_filename ) {
+			echo '<option value="' . $template_filename . '">' . $template_name . '</option>';
+		}
 	
-	echo '<option value="default">' . __( 'MP Stack - 100% Width', 'mp_knapstack' ) . '</option>';
 	
-	foreach ( $templates as $template_name => $template_filename ) {
-		echo '<option value="' . $template_filename . '">' . $template_name . '</option>';
 	}
-	
+	//If this is something else like an EDD Download or a WooCommerce Product
+	else{
+		$templates = get_theme_support( 'post-formats' );
+		$templates = $templates[0];
+		foreach ( $templates as $template_name => $template_filename ) {
+			echo '<option value="' . $template_filename . '">' . apply_filters( 'mp_knapstack_rename_gallery', ucfirst( $template_filename ) ) . '</option>';
+		}
+		echo '<option value="0">' . __( 'Standard', 'mp_knapstack' ) . '</option>';	
+	}
+
 	die();
 			
 }
@@ -23,8 +38,17 @@ function mp_knapstack_set_page_template(){
 	//Check the nonce
 	wp_verify_nonce( $_POST['nonce'], 'mp-knapstack-change-template-' . $_POST['post_id'] );
 	
-	//Change the page template	
-	update_post_meta( $_POST['post_id'], '_wp_page_template', $_POST['template_filename'] );
+	//If this is a page
+	if ( $_POST['post_type'] == 'page' ){
+		//Change the page template	
+		update_post_meta( $_POST['post_id'], '_wp_page_template', $_POST['template_filename'] );
+	}
+	//If this is something other than a page (EDD Download or WooCommerce Product)
+	else{
+		//Change the post format
+		set_post_format( $_POST['post_id'], $_POST['template_filename'] );
+		update_post_meta( $_POST['post_id'], 'post_format', $_POST['template_filename'] );
+	}
 	
 	echo json_encode( array( true ) );
 	
