474e312728d70e96310d45f1051fd18a6245ab36
Phil Johnston <support@mintplugins.com>
2015-11-10 13:52:06 -0500

    1.0.7.6 Version Bump

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 */
 // Theme version
 if( !defined( 'MP_KNAPSTACK_VERSION' ) )
-	define( 'MP_KNAPSTACK_VERSION', '1.0.7.5' );
+	define( 'MP_KNAPSTACK_VERSION', '1.0.7.6' );
 
 // Theme Folder Path
 if( !defined( 'MP_KNAPSTACK_THEME_DIR' ) )
