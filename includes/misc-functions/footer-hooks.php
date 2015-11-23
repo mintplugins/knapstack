127a1227714eb604e8df90dda425ebbcf3a3ab34
Phil Johnston <support@mintplugins.com>
2015-11-22 19:29:41 -0500

    Exit Footer Stack area if set to 'none'

--- includes/misc-functions/footer-hooks.php
+++ includes/misc-functions/footer-hooks.php
@@ -13,6 +13,11 @@ function mp_knapstack_footer_stack_callback(){
 	
 	$footer_stack_id = get_theme_mod('mp_stacks_footer_stack'); 
 	
+	//If the footer is set to be none
+	if ( $footer_stack_id == 'none' ){
+		return NULL;	
+	}
+	
 	//Check if a footer has been saved to the 'mp_stacks_footer_stack' theme mod
 	if ( !empty( $footer_stack_id  ) && $footer_stack_id != 'none' ){
 		echo mp_stack( $footer_stack_id );
@@ -20,6 +25,7 @@ function mp_knapstack_footer_stack_callback(){
 	//If no footer was saved there, check if one was saved to mp_knapstack_footer_stack (for backwards compaitibility before this option was changed to 'mp_stacks_footer_stack')
 	else{
 		$footer_stack_id = get_theme_mod('mp_knapstack_footer_stack'); 	
+		
 		if ( !empty( $footer_stack_id  ) && $footer_stack_id != 'none' ){
 			echo mp_stack( $footer_stack_id );
 		}
