33c4436a09777b67100f0c981ad9b0836f9e63f5
Phil Johnston <support@mintplugins.com>
2016-02-11 16:22:36 -0500

    Proper function naming for sidebar registration

--- includes/misc-functions/register-sidebars.php
+++ includes/misc-functions/register-sidebars.php
@@ -1,26 +1,50 @@
 <?php
 /**
- * Register widgetized areas, including two sidebars and four widget-ready columns in the footer.
- * Deprecated because users can now use the widget add-on for MP Stacks.
+ * Register widgetized areas
  *
- * To override malachi_widgets_init() in a child theme, remove the action hook and add your own
- * function tied to the init hook.
- *
- * @since Twenty Ten 1.0
+ * @since Knapstack Theme 1.0
  * @uses register_sidebar
  */
-function malachi_widgets_init() {
-	// Home page area, located on the main area of the homepage template
-	register_sidebar( array(
-		'name' => __( 'Footer Widgets', 'mp_knapstack' ),
-		'id' => 'footer-widgets',
-		'description' => __( 'Widgets for the Footer', 'mp_knapstack' ),
-		'before_widget' => '<div class="footer-widget"><div class="footer-widget-inner">',
-		'after_widget' => '</div></div>',
-		'before_title' => '<div class="title"><h3>',
-		'after_title' => '</h3></div>',
-	) );
+function mp_knapstack_widgets_init() {
+	
+	//Setup the sidebar that appears beside blog posts
+		$knapstack_sidebar_args = array(
+			'name'          => __( 'Post Sidebar', 'mp_knapstack' ),
+			'id'            => 'knapstack-post-sidebar',
+			'description'   => __( 'This sidebar appears beside blog posts', 'mp_knapstack' ),
+			'class'         => '',
+			'before_widget' => '<div class="knapstack-widgets-item">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<div class="knapstack-widgets-title">',
+			'after_title'   => '</div>' 
+		);
+		register_sidebar( $knapstack_sidebar_args );
+		
+		//Setup the sidebar that appears beside blog posts
+		$knapstack_sidebar_args = array(
+			'name'          => __( 'Page Sidebar', 'mp_knapstack' ),
+			'id'            => 'knapstack-page-sidebar',
+			'description'   => __( 'This sidebar appears beside pages set to be 600px wide.', 'mp_knapstack' ),
+			'class'         => '',
+			'before_widget' => '<div class="knapstack-widgets-item">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<div class="knapstack-widgets-title">',
+			'after_title'   => '</div>' 
+		);
+		register_sidebar( $knapstack_sidebar_args );
+		
+		//Setup the sidebar that appears beside blog posts
+		$knapstack_sidebar_args = array(
+			'name'          => __( 'Category Sidebar', 'mp_knapstack' ),
+			'id'            => 'knapstack-category-sidebar',
+			'description'   => __( 'This sidebar appears beside category pages.', 'mp_knapstack' ),
+			'class'         => '',
+			'before_widget' => '<div class="knapstack-widgets-item">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<div class="knapstack-widgets-title">',
+			'after_title'   => '</div>' 
+		);
+		register_sidebar( $knapstack_sidebar_args );
 	
 }
-/** Register sidebars by running malachi_widgets_init() on the widgets_init hook. */
-//add_action( 'widgets_init', 'malachi_widgets_init' );	
+add_action( 'widgets_init', 'mp_knapstack_widgets_init' );	
