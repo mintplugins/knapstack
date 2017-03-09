1a5102094548c8421e8d480ee4e33eeac9dc94e2
Phil Johnston <support@mintplugins.com>
2017-03-09 10:38:33 -0500

    Make wp embedded youtube vids responsive

--- includes/misc-functions/theme-setup.php
+++ includes/misc-functions/theme-setup.php
@@ -14,33 +14,33 @@ if ( ! isset( $content_width ) )
  */
 if ( ! function_exists( 'mp_knapstack_setup' ) ):
 	function mp_knapstack_setup() {
-		
+
 		// This theme uses post thumbnails
 		add_theme_support( 'post-thumbnails' );
-	
+
 		// Add default posts and comments RSS feed links to head
 		add_theme_support( 'automatic-feed-links' );
-		
+
 		//Add Post format for stacks - We use the gallery one and change the name to "Full-Width"
 		add_theme_support( 'post-formats', array( 'gallery' ) );
-		
+
 		//Add Post Formats to Downloads
 		add_post_type_support( 'download', 'post-formats' );
-		
+
 		//Add Post Formats to Woo Products
 		add_post_type_support( 'product', 'post-formats' );
-		
+
 		//Declare support for WooCommerce
 		add_theme_support( 'woocommerce' );
-		
+
 		//Declare support for the Title tag
 		add_theme_support( 'title-tag' );
-						
+
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'primary' => __( 'Primary Navigation', 'mp_knapstack' ),
 		) );
-	
+
 	}
 endif;
 
@@ -51,9 +51,9 @@ add_action( 'after_setup_theme', 'mp_knapstack_setup' );
  * Set content width of site
  */
 function mp_knapstack_viewport(){
-	
+
 	$responsive_check = get_theme_mod('mp_knapstack_responsive_off');
-	
+
 	//If we are using responsive
 	if ( empty( $responsive_check ) ){
 		echo '<meta name="viewport" content="width=device-width" />';
@@ -78,19 +78,19 @@ add_filter( 'esc_html', 'mp_knapstack_rename_post_formats' );
 add_filter( 'mp_knapstack_rename_gallery', 'mp_knapstack_rename_post_formats' );
 
 //Rename gallery in posts list table
-function mp_knapstack_live_rename_formats() { 
+function mp_knapstack_live_rename_formats() {
     global $current_screen;
 
     if ( isset( $current_screen->id ) && $current_screen->id == 'edit-post' ) { ?>
         <script type="text/javascript">
         jQuery('document').ready(function() {
 
-            jQuery("span.post-state-format").each(function() { 
+            jQuery("span.post-state-format").each(function() {
                 if ( jQuery(this).text() == "Gallery" )
-                    jQuery(this).text("<?php echo __('Full-Width (Use for Stacks)', 'mp_knapstack'); ?>");             
+                    jQuery(this).text("<?php echo __('Full-Width (Use for Stacks)', 'mp_knapstack'); ?>");
             });
 
-        });      
+        });
         </script>
 <?php }
 }
@@ -104,15 +104,15 @@ add_action('get_header', 'mp_knapststack_filter_head');
 
 //Function we don't call but to ease Theme Check's mind about stuff we don't actually need
 function mp_knapstack_theme_check_appeaser(){
-	the_post_thumbnail();	
+	the_post_thumbnail();
 	add_theme_support( "custom-header", NULL );
-	add_theme_support( "custom-background", NULL ); 
+	add_theme_support( "custom-background", NULL );
 }
 
 //Backwards compatibility for the title tag.
 if ( ! function_exists( '_wp_render_title_tag' ) ) {
 	function theme_slug_render_title() { ?>
-		<title><?php wp_title( '|', true, 'right' ); ?></title><?php 
+		<title><?php wp_title( '|', true, 'right' ); ?></title><?php
 	}
 	add_action( 'wp_head', 'theme_slug_render_title' );
 }
@@ -122,7 +122,7 @@ if ( ! function_exists( '_wp_render_title_tag' ) ) {
  */
 function mp_knapstack_woocommerce_image_dimensions() {
 	global $pagenow;
- 
+
 	if ( ! isset( $_GET['activated'] ) || $pagenow != 'themes.php' ) {
 		return;
 	}
@@ -152,3 +152,11 @@ function mp_knapstack_remove_wc_breadcrumbs() {
     remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
 }
 add_action( 'init', 'mp_knapstack_remove_wc_breadcrumbs' );
+
+/*  Add responsive container to embeds
+/* ------------------------------------ */
+function mp_knapstack_fix_embeds( $html ) {
+    return '<div class="video-container">' . $html . '</div>';
+}
+add_filter( 'embed_oembed_html', 'mp_knapstack_fix_embeds', 10, 3 );
+add_filter( 'video_embed_html', 'mp_knapstack_fix_embeds' ); // Jetpack
